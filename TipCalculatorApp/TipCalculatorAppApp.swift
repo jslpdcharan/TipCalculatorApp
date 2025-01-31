ba03d9e034fb78a506a33e1615a5b11582503639
jslpdcharan <59231349+jslpdcharan@users.noreply.github.com>
2025-01-31 07:56:45 -0600

    Initial Commit

---
+++ TipCalculatorApp/TipCalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorAppApp.swift
+//  TipCalculatorApp
+//
+//  Created by Satyanarayana on 31/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
